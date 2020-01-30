d4509047d1b49efff6dd28fbce03e95267b4d01e
Luke Olsen <lukeaolsen@gmail.com>
2020-01-29 22:07:08 -0500

    clean up server

--- server.js
+++ server.js
@@ -12,24 +12,9 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
-
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
-});
-
 app.get('/api/conference/:conference', async (req, res) => {
-  console.log(req.params.conference)
   let teamData
-  console.log("call before")
   teamData = await teams.sendTeams(req.params.conference)
-  console.log(teamData)
-  console.log("after")
   res.json(teamData)
 })
 
