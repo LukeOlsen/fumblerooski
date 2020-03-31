4512c9f26604d0f02b7e179303d2bb0d4bb22012
Luke Olsen <lukeaolsen@gmail.com>
2020-03-31 17:53:39 -0400

    getting game history for individual teams from db

--- src/client/src/actions/Games/GamesAPI.js
+++ src/client/src/actions/Games/GamesAPI.js
@@ -1,2 +1,14 @@
 import axios from "axios";
-import { setTeamData } from "../index";
+
+export function getAllGames(payload) {
+  console.log(payload);
+  return (dispatch, getState) => {
+    axios.get(`/api/games/history/${payload}`).then(res => {
+      console.log(res);
+    });
+  };
+}
+
+export function getMatchupHistory(payload) {
+  console.log(payload);
+}
--- src/client/src/components/Games/Games.js
+++ src/client/src/components/Games/Games.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { getAllGames, getMatchupHistory } from "../../actions/Games/GamesAPI";
 
 const mapDispatchToProps = dispatch => {
-  return {};
+  return {
+    getAllGames: team => dispatch(getAllGames(team)),
+    getMatchupHistory: (team1, team2) =>
+      dispatch(getMatchupHistory(team1, team2))
+  };
 };
 
 const mapStateToProps = state => {
@@ -16,12 +23,13 @@ class Games extends Component {
     super(props);
 
     this.state = {
-      firstTeam: "",
-      secondTeam: ""
+      firstTeam: "Select",
+      secondTeam: "Select"
     };
 
     this.handleFirstTeamChange = this.handleFirstTeamChange.bind(this);
     this.handleSecondTeamChange = this.handleSecondTeamChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleFirstTeamChange(event) {
@@ -32,6 +40,24 @@ class Games extends Component {
     this.setState({ secondTeam: event.target.value });
   }
 
+  handleSubmit(event) {
+    if (
+      (this.state.firstTeam !== null) |
+      ("Select" & (this.state.secondTeam == null)) |
+      "Select"
+    ) {
+      console.log("get all");
+      this.props.getAllGames(this.state.firstTeam);
+    } else if (
+      (this.state.firstTeam !== null) |
+      ("Select" & (this.state.secondTeam !== null)) |
+      "Select"
+    ) {
+      this.props.getMatchupHistory(this.state.firstTeam, this.state.secondTeam);
+    }
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="p-2 pl-10 flex-grow max-h-full overflow-y-scroll flex-row">
@@ -39,29 +65,47 @@ class Games extends Component {
           <h1>Welcome to the match ups</h1>
         </div>
         <div>
-          <form>
-            <select
-              className="text-black m-10 w-30"
-              value={this.state.firstTeam}
-              onChange={this.handleFirstTeamChange}
-            >
-              <option selected>select</option>
-              {this.props.teamsList.map(team => {
-                return <option key={team.school}>{team.school}</option>;
-              })}
-            </select>
-            <select
-              className="text-black m-10 w-30"
-              value={this.state.secondTeam}
-              onChange={this.handleSecondTeamChange}
-            >
-              <option selected>select</option>
-              {this.props.teamsList.map((team, id) => {
-                return (
-                  <option key={team.school + team.id}>{team.school}</option>
-                );
-              })}
-            </select>
+          <form onSubmit={this.handleSubmit}>
+            <div className="inline-block relative w-64 rounded">
+              <select
+                className="block leading-tight text-black m-10 w-30 appearance-none py-1 px-2 pr-12 rounded"
+                value={this.state.firstTeam}
+                onChange={this.handleFirstTeamChange}
+              >
+                <option selected>Select</option>
+                {this.props.teamsList.map(team => {
+                  return <option key={team.school}>{team.school}</option>;
+                })}
+              </select>
+              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <FontAwesomeIcon icon={faAngleDown} />
+              </div>
+            </div>
+            <div className="inline-block relative w-64 rounded">
+              <select
+                className="block leading-tight text-black m-10 w-30 appearance-none py-1 px-2 pr-12 rounded"
+                value={this.state.secondTeam}
+                onChange={this.handleSecondTeamChange}
+              >
+                <option selected>Select</option>
+                {this.props.teamsList.map((team, id) => {
+                  return (
+                    <option
+                      disabled={
+                        this.state.firstTeam === team.school ? true : null
+                      }
+                      key={team.school + team.id}
+                    >
+                      {team.school}
+                    </option>
+                  );
+                })}
+              </select>
+              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <FontAwesomeIcon icon={faAngleDown} />
+              </div>
+            </div>
+            <input className="mx-10" type="submit" value="Submit" />
           </form>
         </div>
         <div>{this.state.firstTeam}</div>
