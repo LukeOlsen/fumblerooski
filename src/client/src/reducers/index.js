bce677e20f641ac8ee82e966f037b344bf1588e8
Luke Olsen <lukeaolsen@gmail.com>
2020-04-01 17:16:12 -0400

    adding games reducer to combine reducers.

--- src/client/src/reducers/index.js
+++ src/client/src/reducers/index.js
@@ -1,10 +1,12 @@
-import { combineReducers } from 'redux';
-import teamReducer from './team-reducers';
-import rankingsReducer from './rankings-reducers';
-import recruitingReducer from './recruiting-reducers';
+import { combineReducers } from "redux";
+import teamReducer from "./team-reducers";
+import rankingsReducer from "./rankings-reducers";
+import recruitingReducer from "./recruiting-reducers";
+import gamesReducer from "./game-reducers";
 
 export default combineReducers({
-    teamReducer,
-    rankingsReducer,
-    recruitingReducer
-})
+  teamReducer,
+  rankingsReducer,
+  recruitingReducer,
+  gamesReducer
+});
