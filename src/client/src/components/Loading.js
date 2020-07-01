c4c275504ce2ed73d29f6f22965014c3ddedd159
Luke Olsen <lukeaolsen@gmail.com>
2020-06-30 20:51:58 -0400

    Fixing loading state for teams.

--- src/client/src/components/Loading.js
+++ src/client/src/components/Loading.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Loading = () => {
-  return <div className="h-screen w-full opacity-25 bg-gray-100 z-10"></div>;
+  return <div className="h-screen w-full opacity-75 z-10"></div>;
 };
 
 export default Loading;
--- src/client/src/components/Teams/Team.js
+++ src/client/src/components/Teams/Team.js
@@ -87,90 +87,100 @@ export class Team extends Component {
   render() {
     return (
       <div className="p-2 pl-10 flex-grow max-h-full overflow-y-scroll flex-row min-h-screen">
-        <div className="flex">
-          <div className="flex-auto">
-            <p className="text-4xl">
-              {this.props.match.params.teamName}{" "}
-              {!this.props.isLoading ? this.props.teamInfo[0].mascot : ""}
-            </p>
-          </div>
-          <div className="flex-auto flex text-center">
-            <form
-              className="flex-1 self-center"
-              onSubmit={this.handleYearSubmit}
-            >
-              <div className="inline-block relative w-20 rounded">
-                <select
-                  className="block leading-tight text-black mr-10 w-30 appearance-none py-1 px-2 pr-12 rounded"
-                  value={this.state.teamYear}
-                  onChange={this.handleYearChange}
+        {this.props.isLoading ? (
+          <Loading />
+        ) : (
+          <div>
+            <div className="flex">
+              <div className="flex-auto">
+                <p className="text-4xl">
+                  {this.props.match.params.teamName}{" "}
+                  {!this.props.isLoading ? this.props.teamInfo[0].mascot : ""}
+                </p>
+              </div>
+              <div className="flex-auto flex text-center">
+                <form
+                  className="flex-1 self-center"
+                  onSubmit={this.handleYearSubmit}
                 >
-                  <option>2019</option>
-                  <option>2018</option>
-                  <option>2017</option>
-                  <option>2016</option>
-                  <option>2015</option>
-                  <option>2014</option>
-                  <option>2013</option>
-                  <option>2012</option>
-                  <option>2011</option>
-                  <option>2010</option>
-                  <option>2009</option>
-                  <option>2008</option>
-                  <option>2007</option>
-                  <option>2006</option>
-                  <option>2005</option>
-                  <option>2004</option>
-                  <option>2003</option>
-                  <option>2002</option>
-                  <option>2001</option>
-                  <option>2000</option>
-                </select>
-                <div className="pointer-events-none absolute inset-y-0 right-0 left-2 flex items-center text-gray-700">
-                  <FontAwesomeIcon icon={faAngleDown} />
+                  <div className="inline-block relative w-20 rounded">
+                    <select
+                      className="block leading-tight text-black mr-10 w-30 appearance-none py-1 px-2 pr-12 rounded"
+                      value={this.state.teamYear}
+                      onChange={this.handleYearChange}
+                    >
+                      <option>2019</option>
+                      <option>2018</option>
+                      <option>2017</option>
+                      <option>2016</option>
+                      <option>2015</option>
+                      <option>2014</option>
+                      <option>2013</option>
+                      <option>2012</option>
+                      <option>2011</option>
+                      <option>2010</option>
+                      <option>2009</option>
+                      <option>2008</option>
+                      <option>2007</option>
+                      <option>2006</option>
+                      <option>2005</option>
+                      <option>2004</option>
+                      <option>2003</option>
+                      <option>2002</option>
+                      <option>2001</option>
+                      <option>2000</option>
+                    </select>
+                    <div className="pointer-events-none absolute inset-y-0 right-0 left-2 flex items-center text-gray-700">
+                      <FontAwesomeIcon icon={faAngleDown} />
+                    </div>
+                  </div>
+                </form>
+                <div className="flex-1 text-2xl self-center">
+                  {!this.props.isLoading
+                    ? this.props.teamInfo[0].conference
+                    : ""}
+                </div>
+                <div className="flex-1 text-2xl self-center">
+                  {!this.props.isLoading ? this.props.teamInfo[0].division : ""}
+                </div>
+                <div className="flex-1 text-2xl self-center">
+                  {!this.props.isLoading
+                    ? this.props.teamInfo[0].teamRecord[0].total_wins +
+                      "-" +
+                      this.props.teamInfo[0].teamRecord[0].total_losses
+                    : ""}
                 </div>
               </div>
-            </form>
-            <div className="flex-1 text-2xl self-center">
-              {!this.props.isLoading ? this.props.teamInfo[0].conference : ""}
             </div>
-            <div className="flex-1 text-2xl self-center">
-              {!this.props.isLoading ? this.props.teamInfo[0].division : ""}
+            <div className="flex flex-wrap flex-col md:flex-row">
+              <div className="bg-gray-700 rounded border-black h-64 w-full sm:flex-1 md:flex-1/3 m-2 shadow-lg">
+                {!this.props.isLoading ? (
+                  <PPALine PPA={this.props.PPA} />
+                ) : null}
+              </div>
             </div>
-            <div className="flex-1 text-2xl self-center">
-              {!this.props.isLoading
-                ? this.props.teamInfo[0].teamRecord[0].total_wins +
-                  "-" +
-                  this.props.teamInfo[0].teamRecord[0].total_losses
-                : ""}
+            <div className="flex">
+              <div className="bg-gray-700 rounded m-2 h-64 sm:flex-1 md:flex-1/3">
+                {!this.props.isLoading ? (
+                  <SimpleMatchup props={this.props.yearRecord} />
+                ) : null}
+              </div>
+              <div className="bg-gray-700 rounded m-2 h-64 sm:flex-1 md:flex-1/3">
+                {!this.props.isLoading ? (
+                  <BCRPieChart data={this.props.BCR} />
+                ) : null}
+              </div>
             </div>
+            <div className="bg-gray-700 rounded m-2">
+              {this.props.isLoading ? (
+                ""
+              ) : (
+                <RecruitsTable recruits={[this.props.recruits]} />
+              )}
+            </div>
+            {this.props.isLoading ? <Loading /> : null}
           </div>
-        </div>
-        <div className="flex flex-wrap flex-col md:flex-row">
-          <div className="bg-gray-700 rounded border-black h-64 w-full sm:flex-1 md:flex-1/3 m-2 shadow-lg">
-            {!this.props.isLoading ? <PPALine PPA={this.props.PPA} /> : null}
-          </div>
-        </div>
-        <div className="flex">
-          <div className="bg-gray-700 rounded m-2 h-64 sm:flex-1 md:flex-1/3">
-            {!this.props.isLoading ? (
-              <SimpleMatchup props={this.props.yearRecord} />
-            ) : null}
-          </div>
-          <div className="bg-gray-700 rounded m-2 h-64 sm:flex-1 md:flex-1/3">
-            {!this.props.isLoading ? (
-              <BCRPieChart data={this.props.BCR} />
-            ) : null}
-          </div>
-        </div>
-        <div className="bg-gray-700 rounded m-2">
-          {this.props.isLoading ? (
-            ""
-          ) : (
-            <RecruitsTable recruits={[this.props.recruits]} />
-          )}
-        </div>
-        {this.props.isLoading ? <Loading /> : null}
+        )}
       </div>
     );
   }
